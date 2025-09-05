e7547659f00cc2afe57390b81dcecbb45c5cd20a
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2025-09-05 22:23:49 +0530

    dev: added base structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+export default function Home() {
+
+  return (
+
+    <div className=" min-h-screen relative space-y-4 flex flex-col justify-center items-center" >
+
+    </div>
+
+
+  );
+}
